22210f9ca006d219581459a5f7d61be204df7b57
Joel Sonoda <joelsonoda@gmail.com>
2017-09-27 15:58:36 -0600

    Leave environment variables intact

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+echo "Running startup scripts"
 find /opt/init/startup -type f -name *.sh -exec chmod +x {} \;
 find /opt/init/startup -type f -name *.sh -print -exec exec {} \;
+echo "Startup scripts completed"
 /usr/bin/runsvdir -P /etc/service
