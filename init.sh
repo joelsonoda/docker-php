22f49ca7e63ff8e405daf5ab9bb2da56cdf49023
Joel Sonoda <joelsonoda@gmail.com>
2017-09-29 11:17:47 -0600

    Update the init script to properly execute the scripts

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 echo "Running startup scripts"
-find /opt/init/startup -type f -name *.sh -exec chmod +x {} \;
-find /opt/init/startup -type f -name *.sh -print -exec {} \;
+find /opt/init/startup -type f -name *.sh -print -exec chmod +x {} \; -exec {} \;
 echo "Startup scripts completed"
 /usr/bin/runsvdir -P /etc/service
